cdfc8bb4676e4886d4d2187b6ba99beb9bbb16ea
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-04 18:14:10 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ Hello
 Typ
  World
 <div>
-<img src ="" alt="hello"/ >
+<img src ="https://sun9-40.userapi.com/impg/x-ZbdRAEPFC8tRHOAtN7PMdvZQW757PYD3yVRg/2DJpf20xO1U.jpg?size=852x852&quality=96&sign=c331625e" alt="hello"/ >
 </div>
 </div>
 
