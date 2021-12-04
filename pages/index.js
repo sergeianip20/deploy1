76edb9ccc24f818a0719d15daa1139596c3cda84
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-04 20:11:36 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@
 export default function Home() {
 return <div>
 <div>
-<Image src="https://sun9-40.userapi.com/impg/x-ZbdRAEPFC8tRHOAtN7PMdvZQW757PYD3yVRg/2DJpf20xO1U.jpg?size=852x852&quality=96&sign=c331625e" width={500} height={100} layout="fixed"/>
+<Image src="/Chr.jpg" width={500} height={100} layout="fixed"/>
 </div>
  Deploy
 Hello
