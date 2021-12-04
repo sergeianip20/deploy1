b4bc5f3acf3a9f6f72e30fc90a1c4cc95a64928c
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-04 22:08:05 +0400

    Update and rename pages/index.js to страницы/index.js

--- страницы/index.js
+++ страницы/index.js
@@ -1,10 +1,10 @@
  import Image from 'next/image'
  import Chr from './Chr.jpg'
-
+ import s from './../styles/Home.module.css
 
 
 export default function Home() {
-return <div>
+return <div className={s.deploy}>
 <div>
 <Image src="/Chr.jpg" width={100} height={100}/>
 </div>
