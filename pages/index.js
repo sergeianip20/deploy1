abab3a4d935fb4e36ab561010686ca51ef7bd7b4
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-04 22:09:48 +0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+ import Chr from './Chr.jpg'
+ import s from './../styles/Home.module.css
+
+
+export default function Home() {
+return <div className={s.deploy}>
+<div>
+<Image src="/Chr.jpg" width={100} height={100}/>
+</div>
+<div>
+<img src='https://images.app.goo.gl/w3vBQ182tei3JAH9A' />
+</div>
+<div>
+<img src={Chr} />
+</div>
+ Deploy
+Hello
+2
+Typ
+ World
+Туоул dhdjj
+
+</div>
+
+}
