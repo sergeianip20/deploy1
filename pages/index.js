f83801dfc5368cb8c81b2a2260334e5029531b3f
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-04 19:04:15 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
- import Image from 'next/image' 
+ 
   
 
 
@@ -11,9 +11,6 @@ Hello
 2
 Typ
  World
-<Image src ="https://sun9-40.userapi.com/impg/x-ZbdRAEPFC8tRHOAtN7PMdvZQW757PYD3yVRg/2DJpf20xO1U.jpg?size=852x852&quality=96&sign=c331625e" 
-width="400" height="250" layout="fill"/>
-<img src ="https://sun9-40.userapi.com/impg/x-ZbdRAEPFC8tRHOAtN7PMdvZQW757PYD3yVRg/2DJpf20xO1U.jpg?size=852x852&quality=96&sign=c331625e" alt="hello"/ >
 
 </div>
 
