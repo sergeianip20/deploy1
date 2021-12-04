f8c65627fb5f2661a1e34a2c3ab4e91a360111c3
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-04 19:24:15 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
- 
+ import Image from 'next/image'
   
 
 
 
 export default function Home() {
 return <div>
-
+<Image src="https://sun9-40.userapi.com/impg/x-ZbdRAEPFC8tRHOAtN7PMdvZQW757PYD3yVRg/2DJpf20xO1U.jpg?size=852x852&quality=96&sign=c331625e" width="500" height="100" layout="fill"/>
  Deploy
 Hello
 2
