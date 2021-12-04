36926f2cfa9d2caa8486fb4bcc24f125775b7f46
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-04 22:29:01 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,14 @@
 import Image from 'next/image'
- import Chr from './Chr.jpg'
+
  import s from './../styles/Home.module.css
 
 
 export default function Home() {
-return <div className={s.deploy}>
+return <div>
 <div>
-<Image src="/Chr.jpg" width={100} height={100}/>
+<Image src="/versel.svg"  alt=" не повезло не фортануло "width={100} height={100}/>
 </div>
-<div>
-<img src='https://images.app.goo.gl/w3vBQ182tei3JAH9A' />
-</div>
-<div>
-<img src={Chr} />
-</div>
+
  Deploy
 Hello
 2
