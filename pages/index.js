f18870e3944f5f0980072858bba3ad4f837b4c21
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-04 15:56:51 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
+  import s from './form.module.css' 
+  import NavBar from './NavBar'
+
+
+
 export default function Home() {
 return <div>
+
+<header> <NavBar/></header>
 Hello
+
  World
 </div>
 
