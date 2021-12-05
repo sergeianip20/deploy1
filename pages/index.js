70515e1ba8afdf71c7dc28cd8386de4c69f07c2b
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-05 14:05:44 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,10 @@ return <div>
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
     <NavDropdown title="Торты" id="collasible-nav-dropdown">
-    <NavDropdown.Item > <Link href= '/secon'><a className = {s.sa}>детские</a></Link> </NavDropdown.Item>
-    <NavDropdown.Item > <Link href= '/'><a className = {s.sa}>заказ</a></Link> </NavDropdown.Item>
+    
         <NavDropdown.Item >Корпоративные</NavDropdown.Item>
         <NavDropdown.Divider />
-        <NavDropdown.Item > <Link href= '/price'><a className = {s.sa}>именные</a></Link> </NavDropdown.Item>
+        
       </NavDropdown>
       <Nav.Link href="#features">Прайс</Nav.Link>
       
