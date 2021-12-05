09ef8b813b40142e4f8d42fb36374e3c630a0c66
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2021-12-05 14:20:15 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,32 +11,7 @@ return <div>
 <div>
 <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
 </div>
-<div>
-<div className={s.navbar_BG}>
-<Navbar collapseOnSelect expand="lg"  variant="dark">
-   
-  <Container>
-  <Navbar.Brand href="#home">VPcake</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-    <NavDropdown title="Торты" id="collasible-nav-dropdown">
-    
-        <NavDropdown.Item >Корпоративные</NavDropdown.Item>
-        <NavDropdown.Divider />
-        
-      </NavDropdown>
-      <Nav.Link href="#features">Прайс</Nav.Link>
-      
-      
-    </Nav>
-    
-  </Navbar.Collapse>
-  </Container>
- 
-</Navbar>
-</div>
-</div>
+
  Deploy
 Hello
 2
